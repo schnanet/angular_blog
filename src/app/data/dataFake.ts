0e1ea328825c4b96eaf55320fc5df542c8c38b0d
schnanet <welintonschneider@hotmail.com>
2024-01-18 23:27:28 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+{
+  "id":"1",
+  "title": "Dragon Ball Z",
+  "description": "Goku derrota Freeza, confira!",
+  "photoCover": "https://olhardigital.com.br/wp-content/uploads/2022/07/Dragon-Ball-Z.webp"
+
+},
+{
+  "id":"2",
+  "title": "Dragon Ball vai ganhar um anime",
+  "description": "O anime inédito será lançado em 2024, confira!",
+  "photoCover": "https://t.ctcdn.com.br/69kcTYGNbfLUJtsl4QidrKil3i0=/1266x712/smart/i810447.jpeg"
+}
+
+
+]
